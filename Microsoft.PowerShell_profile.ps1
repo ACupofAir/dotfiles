68f48c0bcaf92668e1d0a3148b4d31e7cd671c71
ACupofAir <timethoughts@outlook.com>
2023-02-15 01:11:23 +0800

    separte the dirstack module to new repo

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,8 +3,8 @@ Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 Invoke-Expression (&starship init powershell)
-Import-Module -Name ./PSModules/writeBig/ -DisableNameChecking
-Import-Module -Name ./PSModules/dirStack/ -DisableNameChecking
+Import-Module -Name ~/workspace/Personal/dotfiles/PSModules/writeBig/ -DisableNameChecking
+Import-Module -Name ~/workspace/Personal/dirStack/ -DisableNameChecking
 
 ###############################################################################
 ##############################My-Functions#####################################
@@ -52,75 +52,6 @@ function Show-Link-Files($dir_name)
   Get-ChildItem $dir_name -force | Where-Object{$_.LinkType} | Select-Object FullName,LinkType,Target
 }
 
-<<<<<<< HEAD
-function Get-Dir-Stack
-{
-  if ($dirs_stack.Count -eq 0)
-  {
-    Write-Host "The stack is empty, use 'pd dir_name' to push the directory to the stack" -ForegroundColor red
-  } else
-  {
-    Write-Host "Directory Stack:" -ForegroundColor green
-    Write-Host "-------------------------------------------------------------" -ForegroundColor Yellow
-    for ($i = 0; $i -le ($dirs_stack.Count -1); $i += 1)
-    {
-      Write-Host "  $i  " -NoNewline -ForegroundColor Cyan
-      Write-Host "|  " -NoNewline -ForegroundColor Yellow
-      Write-Host "$($dirs_stack[$i])"
-      # Write-Host "-------------------------------------------------------------" -ForegroundColor Yellow
-    }
-    Write-Host "-------------------------------------------------------------" -ForegroundColor Yellow
-    $input_idx = Read-Host "Select which stack you will go (input should be number)"
-    if ($null -ne $input_idx)
-    {
-      Set-Location $dirs_stack[$input_idx]
-    }
-  }
-}
-
-function Set-Dir-Stack($input_text)
-{
-  if ($null -eq $input_text)
-  {
-    # Put current directory into stack
-    $current_dir = $(Get-Location)
-    if ($current_dir -notin $dirs_stack)
-    {
-      $dirs_stack.Add($current_dir)
-    } else
-    {
-      Write-Host "Current directory has been added in the stack" -ForegroundColor blue
-    }
-  } elseif ($input_text -is [ int ] -and $input_text -lt $dirs_stack.Count)
-  {
-    # Go to the directory in the stack whose index is the input_text
-    Set-Location $dirs_stack[$input_text]
-    Write-Host "Jump to " -NoNewline
-    Write-Host "$($dirs_stack[$input_text])" -ForegroundColor Green
-  } elseif ($input_text -is [ string ])
-  {
-    # Go to the directory of input, and put it in the stack
-    Set-Location $input_text -ErrorAction stop
-    $dirs_stack.Add($(Get-Location))
-  } else
-  {
-    # If the input_text is unavailable, print error info and show the current stack
-    Write-Error("The directory stack length is $($dirs_stack.Count)")
-    Get-Dir-Stack
-  }
-}
-
-function Remove-Dir-Stack-Item($index)
-{
-  if ($index -is [ int ] -and $index -lt $dirs_stack.Count)
-  {
-    $dirs_stack.RemoveAt($index)
-  } else
-  {
-    Write-Error("The index $index must less than stack length $($dirs_stack.Count)")
-  }
-}
-
 ###############################################################################
 ################################My-Alias#######################################
 ###############################################################################
@@ -134,6 +65,7 @@ Set-Alias -Name dv Get-Dir-Stack
 Set-Alias -Name pd Set-Dir-Stack
 Set-Alias -Name pp Remove-Dir-Stack-Item
 Set-Alias -Name ll Show-Link-Files
+Set-Alias -Name draw Write-Big-Char
 
 ###############################################################################
 ################################My-Env-Var#####################################
